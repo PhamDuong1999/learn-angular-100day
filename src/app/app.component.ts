95577af295a0b70e684c7eb1e74a7eea8244ad4a
PhamDuong1999 <phamduong221999@gmail.com>
2022-02-03 00:35:08 +0700

    day 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  //name = 'Angular ' + VERSION.major;
+  user = {
+    name: 'Duong',
+    age: 17,
+  };
+  changeUserName(e: any) {
+    this.user.name = e.value;
+  }
 }
